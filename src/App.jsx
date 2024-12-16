e46891fcc33936587ce698c2e08e5204a2605cc5
asliddinnuriddinov <eshmat010@gmail.com>
2024-12-16 12:44:02 +0500

    configured:toastify set up

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from './layout';
 import { TransactionProvider } from './context/TransactionContext';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <TransactionProvider>
+      <ToastContainer position='top-center'/>
       <Layout/>
     </TransactionProvider>
   )
