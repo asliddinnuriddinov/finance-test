0c952273b73547ccc09c9c59646220bbb2e45db1
asliddinnuriddinov <eshmat010@gmail.com>
2024-12-16 08:13:22 +0500

    created:table ui

---
+++ src/ui/Table/index.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Badge } from 'react-bootstrap';
+import Table from 'react-bootstrap/Table';
+
+const CustomTable = ({ headings, data, dataToShow }) => {
+    return (
+        <Table striped hover rounded className='shadow-sm'>
+            <thead>
+                <tr>
+                    {headings.map((heading, index) => (
+                        <th key={index}>{heading}</th>
+                    ))}
+                </tr>
+            </thead>
+            <tbody>
+                {data.map((row, rowIndex) => (
+                    <tr key={rowIndex}>
+                        {dataToShow.map((property, colIndex) => (
+                            <td key={colIndex}>
+                                {property == 'type'?(
+                                    row[property] === 'income' ? (
+                                        <Badge style={{fontSize: '15px'}} bg="success">{row[property]}</Badge>
+                                    ) : (
+                                        <Badge style={{fontSize: '15px'}} bg="danger">{row[property]}</Badge>
+                                    )
+                                ):
+                                property.includes('amount')?(
+                                    property == 'amount' ? 
+                                    `${row[property].toFixed(2)} ${row.currency}`
+                                    :
+                                    `${row[property].toFixed(2)} USD`
+                                ):
+                                (
+                                    row[property] !== undefined ? row[property] : 'N/A'
+                                )}
+                            </td>
+                        ))}
+                    </tr>
+                ))}
+            </tbody>
+        </Table>
+    );
+};
+
+export default CustomTable;
